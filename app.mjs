e15a99fce800d3fa165a9482387aaa3ca0cb5585
Cecilie Røgh Eik <ceciliereik@Cecilies-Air.uia.no>
2024-01-15 14:07:32 +0100

    added colorful joke

---
+++ app.mjs
@@ -0,0 +1,15 @@
+import chalk from "chalk";
+
+console.log(
+  chalk.rgb(207, 147, 136)("Hva") +
+  " " +
+  chalk.rgb(207, 191, 136)("heter") +
+  " " +
+  chalk.rgb(176, 207, 136)("gartneren") +
+  " " +
+  chalk.rgb(136, 192, 207)("til") +
+  " " +
+  chalk.rgb(167, 136, 207)("Lenin?") +
+  " " +
+  chalk.rgb(207, 136, 176)("Plenin!")
+);
